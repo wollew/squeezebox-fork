c359aefce444c74d0c5549859d741c3a1524b976
Wolfgang Miller-Reichling <wolfgang.miller-reichling@1und1.de>
2025-06-18 13:51:26 +0000

    [squeezebox integration] expose datetime of next alarm of players as a timestamp sensor

--- custom_components/squeezebox-fork/sensor.py
+++ custom_components/squeezebox-fork/sensor.py
@@ -13,11 +13,15 @@ from homeassistant.components.sensor import (
 )
 from homeassistant.const import UnitOfTime
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import format_mac
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 from homeassistant.helpers.typing import StateType
 
 from . import SqueezeboxConfigEntry
 from .const import (
+    PLAYER_SENSOR_NEXT_ALARM,
+    SIGNAL_PLAYER_DISCOVERED,
     STATUS_SENSOR_INFO_TOTAL_ALBUMS,
     STATUS_SENSOR_INFO_TOTAL_ARTISTS,
     STATUS_SENSOR_INFO_TOTAL_DURATION,
@@ -27,12 +31,12 @@ from .const import (
     STATUS_SENSOR_OTHER_PLAYER_COUNT,
     STATUS_SENSOR_PLAYER_COUNT,
 )
-from .entity import LMSStatusEntity
+from .entity import LMSStatusEntity, SqueezeboxEntity, SqueezeBoxPlayerUpdateCoordinator
 
 # Coordinator is used to centralize the data updates
 PARALLEL_UPDATES = 0
 
-SENSORS: tuple[SensorEntityDescription, ...] = (
+SERVER_SENSORS: tuple[SensorEntityDescription, ...] = (
     SensorEntityDescription(
         key=STATUS_SENSOR_INFO_TOTAL_ALBUMS,
         state_class=SensorStateClass.TOTAL,
@@ -71,6 +75,13 @@ SENSORS: tuple[SensorEntityDescription, ...] = (
     ),
 )
 
+PLAYER_SENSORS: tuple[SensorEntityDescription, ...] = (
+    SensorEntityDescription(
+        key=PLAYER_SENSOR_NEXT_ALARM,
+        device_class=SensorDeviceClass.TIMESTAMP,
+    ),
+)
+
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -81,16 +92,64 @@ async def async_setup_entry(
 ) -> None:
     """Platform setup using common elements."""
 
+    # Add player sensor entities when player discovered
+    async def _player_discovered(
+        player_coordinator: SqueezeBoxPlayerUpdateCoordinator,
+    ) -> None:
+        _LOGGER.debug(
+            "Setting up sensor entities for player %s, model %s",
+            player_coordinator.player.name,
+            player_coordinator.player.model,
+        )
+
+        entities: list[SqueezeboxSensorEntity] = []
+
+        entities.extend(
+            SqueezeboxSensorEntity(player_coordinator, description)
+            for description in PLAYER_SENSORS
+        )
+
+        async_add_entities(entities)
+
+    entry.async_on_unload(
+        async_dispatcher_connect(hass, SIGNAL_PLAYER_DISCOVERED, _player_discovered)
+    )
+
     async_add_entities(
         ServerStatusSensor(entry.runtime_data.coordinator, description)
-        for description in SENSORS
+        for description in SERVER_SENSORS
     )
 
 
 class ServerStatusSensor(LMSStatusEntity, SensorEntity):
-    """LMS Status based sensor from LMS via cooridnatior."""
+    """LMS Status based sensor from LMS via coordinator."""
 
     @property
     def native_value(self) -> StateType:
         """LMS Status directly from coordinator data."""
         return cast(StateType, self.coordinator.data[self.entity_description.key])
+
+
+class SqueezeboxSensorEntity(SqueezeboxEntity, SensorEntity):
+    """Representation of player based sensors."""
+
+    description: SensorEntityDescription
+
+    def __init__(
+        self,
+        coordinator: SqueezeBoxPlayerUpdateCoordinator,
+        description: SensorEntityDescription,
+    ) -> None:
+        """Initialize the SqueezeBox Button."""
+        super().__init__(coordinator)
+        self.description = description
+        self._attr_translation_key = description.key.replace(" ", "_")
+        self._attr_unique_id = f"{format_mac(self._player.player_id)}_{description.key}"
+
+    @property
+    def native_value(self) -> StateType:
+        """Sensor value directly from player coordinator."""
+
+        val = getattr(self.coordinator.player, self.description.key, None)
+        _LOGGER.debug("%s %s", self._attr_unique_id, val)
+        return cast(StateType, val)
