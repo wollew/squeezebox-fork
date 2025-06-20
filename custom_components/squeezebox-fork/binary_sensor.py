8581d5a25b91469f9de139e795633f72b5d1324f
Wolfgang Miller-Reichling <wolfgang.miller-reichling@1und1.de>
2025-06-20 12:58:01 +0000

    add player sensors for upcoming and current alarms

--- custom_components/squeezebox-fork/binary_sensor.py
+++ custom_components/squeezebox-fork/binary_sensor.py
@@ -11,11 +11,20 @@ from homeassistant.components.binary_sensor import (
 )
 from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import format_mac
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
-from . import SqueezeboxConfigEntry
-from .const import STATUS_SENSOR_NEEDSRESTART, STATUS_SENSOR_RESCAN
-from .entity import LMSStatusEntity
+from . import SqueezeboxConfigEntry, SqueezeBoxPlayerUpdateCoordinator
+from .const import (
+    PLAYER_SENSOR_ALARM_ACTIVE,
+    PLAYER_SENSOR_ALARM_SNOOZE,
+    PLAYER_SENSOR_ALARM_UPCOMING,
+    SIGNAL_PLAYER_DISCOVERED,
+    STATUS_SENSOR_NEEDSRESTART,
+    STATUS_SENSOR_RESCAN,
+)
+from .entity import LMSStatusEntity, SqueezeboxEntity
 
 # Coordinator is used to centralize the data updates
 PARALLEL_UPDATES = 0
@@ -32,6 +41,21 @@ SENSORS: tuple[BinarySensorEntityDescription, ...] = (
     ),
 )
 
+PLAYER_SENSORS: tuple[BinarySensorEntityDescription, ...] = (
+    BinarySensorEntityDescription(
+        key=PLAYER_SENSOR_ALARM_UPCOMING,
+        device_class=BinarySensorDeviceClass.RUNNING,
+    ),
+    BinarySensorEntityDescription(
+        key=PLAYER_SENSOR_ALARM_ACTIVE,
+        device_class=BinarySensorDeviceClass.RUNNING,
+    ),
+    BinarySensorEntityDescription(
+        key=PLAYER_SENSOR_ALARM_SNOOZE,
+        device_class=BinarySensorDeviceClass.RUNNING,
+    ),
+)
+
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -42,6 +66,28 @@ async def async_setup_entry(
 ) -> None:
     """Platform setup using common elements."""
 
+    # Add player sensor entities when player discovered
+    async def _player_discovered(
+        player_coordinator: SqueezeBoxPlayerUpdateCoordinator,
+    ) -> None:
+        _LOGGER.debug(
+            "Setting up binary sensor entities for player %s, model %s",
+            player_coordinator.player.name,
+            player_coordinator.player.model,
+        )
+
+        entities: list[SqueezeboxBinarySensorEntity] = []
+
+        entities.extend(
+            SqueezeboxBinarySensorEntity(player_coordinator, description)
+            for description in PLAYER_SENSORS
+        )
+
+        async_add_entities(entities)
+
+    entry.async_on_unload(
+        async_dispatcher_connect(hass, SIGNAL_PLAYER_DISCOVERED, _player_discovered)
+    )
     async_add_entities(
         ServerStatusBinarySensor(entry.runtime_data.coordinator, description)
         for description in SENSORS
@@ -49,9 +95,31 @@ async def async_setup_entry(
 
 
 class ServerStatusBinarySensor(LMSStatusEntity, BinarySensorEntity):
-    """LMS Status based sensor from LMS via cooridnatior."""
+    """LMS Status based sensor from LMS via coordinator."""
 
     @property
     def is_on(self) -> bool:
         """LMS Status directly from coordinator data."""
         return bool(self.coordinator.data[self.entity_description.key])
+
+
+class SqueezeboxBinarySensorEntity(SqueezeboxEntity, BinarySensorEntity):
+    """Representation of player based binary sensors."""
+
+    description: BinarySensorEntityDescription
+
+    def __init__(
+        self,
+        coordinator: SqueezeBoxPlayerUpdateCoordinator,
+        description: BinarySensorEntityDescription,
+    ) -> None:
+        """Initialize the SqueezeBox Button."""
+        super().__init__(coordinator)
+        self.description = description
+        self._attr_translation_key = description.key.replace(" ", "_")
+        self._attr_unique_id = f"{format_mac(self._player.player_id)}_{description.key}"
+
+    @property
+    def is_on(self) -> bool:
+        """Return the state of the binary sensor."""
+        return bool(getattr(self.coordinator.player, self.description.key, None))
